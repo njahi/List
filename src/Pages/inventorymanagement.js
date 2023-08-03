f1a112bc00ed366ccda5445675ba233c0e4c90b4
Humphrey Gatinu <njahigatinu@gnail.com>
2023-08-03 16:47:03 +0300

    added a component

--- src/Pages/inventorymanagement.js
+++ src/Pages/inventorymanagement.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import AssetCard from "../components/AssetCard";
 
 function inventorymanagement() {
-  return <div></div>;
+  return <AssetCard />;
 }
 export default inventorymanagement;
---
+++ src/components/AssetCard.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import "./AssetCard.css";
+
+function ProductCard() {
+  const [assets, setAssets] = useState([]);
+
+  // Function to fetch assets from the server
+  const fetchAssets = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/asset");
+      const data = await response.json();
+      setAssets(data);
+    } catch (error) {
+      console.error("Error fetching assets:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAssets();
+  }, []);
+
+  return (
+    <div>
+      <Row xs={1} md={4} className="g-4">
+        {assets.map((product) => (
+          <Col align="center">
+            <Card key={product.id} className="product-card">
+              <Card.Body className="product-details">
+                <Card.Title className="product-name">{product.name}</Card.Title>
+                <Card.Text className="product-value">
+                  ${product.value}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+}
+
+export default ProductCard;

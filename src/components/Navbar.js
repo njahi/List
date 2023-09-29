2427015c6a07aeef357ac9bb9dbbc6e9eb5f5719
Humphrey Gatinu <njahigatinu@gmail.com>
2023-09-29 19:06:39 +0300

    added a serach bar without logic functionalities

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,32 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <img src="./Furniture.jpg" alt="icon" className="Dash-image" />
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/service">Service</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className='nav-bar'>
+        <img
+          src='./Furniture.jpg'
+          alt='icon'
+          className='Dash-image'
+        />
+        <input
+          className='search'
+          type='text'
+          placeholder='Search Asset...'
+        />
+
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/service'>Service</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
 export default Navbar;

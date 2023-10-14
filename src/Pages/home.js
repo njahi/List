8ccb68e6c8cc65a7ed788e7166da4e7cc72903f2
Humphrey Gatinu <njahigatinu@gmail.com>
2023-10-14 17:33:54 +0300

    image setter

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -4,8 +4,31 @@ import Dashboard from "../components/Dashboard";
 function home() {
   return (
     <>
-      <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginRight: "100px",
+        }}>
         <Dashboard />
+        <div>
+          <div
+            style={{
+              fontSize: "large",
+              fontFamily: "sans-serif",
+              backgroundColor: "grey",
+              paddingBottom: "5px",
+              borderRadius: "0.9rem",
+              width: "75.8rem",
+            }}>
+            <h1>Welcome to your World</h1>
+          </div>
+          <img
+            src='Furniture.jpg'
+            alt='home'
+            style={{ height: "35.3rem", width: "75.8rem" }}
+          />
+        </div>
       </div>
     </>
   );

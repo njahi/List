9bfe3b5b4a99e080978de467a5f66bd0fc246024
Humphrey Gatinu <njahigatinu@gmail.com>
2023-11-07 11:01:33 +0300

    modification of the LogOut hook

--- src/hooks/useLogOut.js
+++ src/hooks/useLogOut.js
@@ -15,7 +15,7 @@ export function useLogOut() {
   } = useMutation({
     mutationFn: logOut,
     onSuccess: () => {
-      toast.success("Logged out successful");
+      toast.success("Logged out successfully");
       navigate("/login");
     },
     onError: () => {

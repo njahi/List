ce086f9ff28fa3036273157ed7b052c2f6e827f6
Humphrey Gatinu <njahigatinu@gmail.com>
2023-11-07 10:55:26 +0300

    LogOut functionality

--- src/components/LogOut.js
+++ src/components/LogOut.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { useLogOut } from "../hooks/useLogOut";
+import { AiOutlineLogout } from "react-icons/ai";
+import toast from "react-hot-toast";
 
 export default function LogOut() {
-  return <button>Logout</button>;
+  const { logOutAPI, isLoading, error } = useLogOut();
+  function handleLogOut() {
+    logOutAPI();
+  }
+  if (isLoading) {
+    return <p>Logging Out...</p>;
+  }
+  if (error) {
+    toast.error("error logging out");
+  }
+
+  return (
+    <button onClick={handleLogOut}>
+      <AiOutlineLogout />
+    </button>
+  );
 }

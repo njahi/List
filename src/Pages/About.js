837ea18d25b7f1a0f6e2c7a3612398f37d86ae46
Humphrey Gatinu <njahigatinu@gmail.com>
2023-11-19 15:35:52 +0300

    Order page modification

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { PopUp3 } from "../components/PopUp3";
-import ProgressBar from "../components/ProgressBar";
 
 function About() {
   const [show, setShow] = useState(false);

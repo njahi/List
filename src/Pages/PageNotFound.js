6cf8af674b3d21016c759bf1c4318c415d6cb36c
Humphrey Gatinu <njahigatinu@gmail.com>
2023-11-21 16:04:43 +0300

    PageNotFound modification

--- src/Pages/PageNotFound.js
+++ src/Pages/PageNotFound.js
@@ -1,9 +1,53 @@
 import React from "react";
+import { Title, Text, Button, Container, Group } from "@mantine/core";
 
 export default function PageNotFound() {
   return (
-    <div>
-      <h3>Page Not Found :</h3>
-    </div>
+    <Container
+      style={{
+        paddingTop: "rem(80px",
+        paddingBottom: "rem(80px)",
+      }}>
+      <div
+        style={{
+          textAlign: "center",
+          fontWeight: "900",
+          fontSize: "rem(38px",
+          lineHeight: "1",
+          marginBottom: "calc(1.5 * var(--mantine-spacing-xl))",
+          color: "var(--mantine-color-gray-2)",
+        }}>
+        404
+      </div>
+      <Title
+        style={{
+          fontFamily: "Greycliff CF, var(--mantine-font-family)",
+          textAlign: "center",
+          fontWeight: "900",
+          fontSize: "rem(38px)",
+        }}>
+        You have found a secret place.
+      </Title>
+      <Text
+        c='dimmed'
+        size='lg'
+        ta='center'
+        style={{
+          maxWidth: "rem(500px)",
+          margin: "auto",
+          marginTop: " var(--mantine-spacing-xl)",
+          marginBottom: "calc(1.5 * var(--mantine-spacing-xl))",
+        }}>
+        Unfortunately, this is only a 404 page. You may have mistyped the
+        address, or the page has been moved to another URL.
+      </Text>
+      <Group justify='center'>
+        <Button
+          variant='subtle'
+          size='md'>
+          Take me back to home page
+        </Button>
+      </Group>
+    </Container>
   );
 }

f1f3b521d4e0b4162225e9505a580244d7a12d66
Humphrey Gatinu <njahigatinu@gmail.com>
2023-11-25 12:36:36 +0300

    login hook

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,23 @@
+import toast from "react-hot-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { login } from "../services/apiUsers";
+
+export function useLogIn() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { mutate: logInFn, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => login({ email, password }),
+    onSuccess: (user) => {
+      queryClient.setQueryData(["user"], user);
+      toast.success("Log in Successfull");
+      navigate("/home", { replace: true });
+    },
+    onError: (err) => {
+      console.log(err.message);
+      toast.error("Provided email and password are incorect");
+    },
+  });
+
+  return { logInFn, isLoading };
+}

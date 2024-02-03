c0b5e46d9d2f3af7624070cc4e279f1294f304ab
Humphrey Gatinu <njahigatinu@gmail.com>
2024-02-03 16:01:11 +0300

    fetch the users data from the database in supabase in the table Users

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,11 +1,28 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { supabase } from "./supabase";
 
 const UserContext = createContext();
 function UsersProvider({ children }) {
+  const [users, setUsers] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(function () {
+    async function fetchUsers() {
+      try {
+        const res = await supabase.from("Users").select();
+        const data = await res.json();
+        setUsers(data);
+      } catch (error) {
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchUsers();
+  }, []);
   return (
     <UserContext.Provider
       value={{
         users,
+        isLoading,
       }}>
       {children}
     </UserContext.Provider>

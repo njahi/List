03c5dced1115f02c8e868bcf04a8b32f18f35e53
Humphrey Gatinu <njahigatinu@gmail.com>
2024-02-22 11:08:23 +0300

    implemented lazy loading on our single page app routes to be able to optimize bundle size

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { lazy } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import Register from "./Pages/Register";
-import Login from "./Pages/Login";
-import Home from "./Pages/Home";
-import Order from "./Pages/Order";
-import Service from "./Pages/Service";
-import Asset from "./Pages/Asset";
-import Reports from "./Pages/Reports";
-import Inventorymanagement from "./Pages/InventoryManagement";
-import Settings from "./Pages/Settings";
-import PageNotFound from "./Pages/PageNotFound";
 import { UsersProvider } from "./context/userContext";
+const Register = lazy(() => import("./Pages/Register"));
+const Login = lazy(() => import("./Pages/Login"));
+const Home = lazy(() => import("./Pages/Home"));
+const Order = lazy(() => import("./Pages/Order"));
+const Service = lazy(() => import("./Pages/Service"));
+const Asset = lazy(() => import("./Pages/Asset"));
+const Reports = lazy(() => import("./Pages/Reports"));
+const Inventorymanagement = lazy(() => import("./Pages/InventoryManagement"));
+const Settings = lazy(() => import("./Pages/Settings"));
+const PageNotFound = lazy(() => import("./Pages/PageNotFound"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
